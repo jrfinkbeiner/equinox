5c85306115d417d14195e6f7048ffeb008aefb03
Jan Finkbeiner <j.finkbeiner@fz-juelich.de>
2022-04-08 15:51:59 +0000

    add init_funcs to linear

--- equinox/nn/linear.py
+++ equinox/nn/linear.py
@@ -1,11 +1,18 @@
 import math
-from typing import Optional, TypeVar
+from typing import Optional, TypeVar, Callable
 
 import jax
 import jax.random as jrandom
 
 from ..custom_types import Array
 from ..module import Module, static_field
+
+import functools as ft
+
+def init_xavier_uniform(key, shape, in_features, out_features):
+    lim = math.sqrt(6 / (in_features+out_features))
+    params = jrandom.uniform(key, shape, minval=-lim, maxval=lim)
+    return params
 
 
 class Linear(Module):
@@ -16,12 +23,16 @@ class Linear(Module):
     in_features: int = static_field()
     out_features: int = static_field()
     use_bias: bool = static_field()
+    init_func_weights: Optional[Callable]
+    init_func_bias: Optional[Callable]
 
     def __init__(
         self,
         in_features: int,
         out_features: int,
         use_bias: bool = True,
+        init_func_weights: Optional[Callable] = None, 
+        init_func_bias: Optional[Callable] = None,
         *,
         key: "jax.random.PRNGKey"
     ):
@@ -35,18 +46,23 @@ class Linear(Module):
         """
         super().__init__()
         wkey, bkey = jrandom.split(key, 2)
-        lim = 1 / math.sqrt(in_features)
-        self.weight = jrandom.uniform(
-            wkey, (out_features, in_features), minval=-lim, maxval=lim
-        )
+
+        if init_func_weights is None:
+            init_func_weights = ft.partial(init_xavier_uniform, in_features=in_features, out_features=out_features)
+
+        self.weight = init_func_weights(wkey, (out_features, in_features))
         if use_bias:
-            self.bias = jrandom.uniform(bkey, (out_features,), minval=-lim, maxval=lim)
+            if init_func_bias is None:
+                init_func_bias = ft.partial(init_xavier_uniform, in_features=in_features, out_features=out_features)
+            self.bias = init_func_bias(bkey, (out_features,))
         else:
             self.bias = None
 
         self.in_features = in_features
         self.out_features = out_features
         self.use_bias = use_bias
+        self.init_func_weights = init_func_weights
+        self.init_func_bias = init_func_bias
 
     def __call__(
         self, x: Array, *, key: Optional["jax.random.PRNGKey"] = None
